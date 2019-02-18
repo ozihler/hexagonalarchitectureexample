b2a9389c89c84a3550bb4a264c560f8113ad8cc9
ozihler <oliver.zihler@zuehlke.com>
2019-02-18 17:32:07 +0100

    initial setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,43 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
-        InMemoryToDoListRepository repository = new InMemoryToDoListRepository();
-        Console console = new Console();
+        Map<String, List> toDoLists = new HashMap<>();
+        toDoLists.put("Daily Routine", new ArrayList<>(List.of(Item.of("Grocery Shopping"), Item.of("Fitness"))));
 
-        ToDoList toDoList = repository.findByName("Daily Routine");
-        console.show(toDoList);
+        List<Item> toDoList = toDoLists.get("Daily Routine");
+        show(toDoList);
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Item nextItem;
-        while ((nextItem = console.enterNextItemToList()) != null) {
-            toDoList.add(nextItem);
-            repository.save(toDoList);
-            console.show(toDoList);
-        }
+        do {
+            System.out.println("Enter next item to todo list:");
+            String nextLine = reader.readLine();
 
-        console.finish();
+            if (nextLine == null || nextLine.isBlank()) {
+                nextItem = null;
+            } else {
+                nextItem = new Item(nextLine);
+            }
+
+            toDoList.add(nextItem);
+            toDoLists.put("Daily Routine", toDoList);
+            show(toDoList);
+        } while (nextItem != null);
+
+        System.out.println("Finished adding items to todo list");
+    }
+
+    private static void show(List<Item> toDoList) {
+        System.out.println("All items in todo list:");
+        System.out.println("=======================");
+        toDoList.forEach(System.out::println);
+        System.out.println("=======================");
     }
 }
