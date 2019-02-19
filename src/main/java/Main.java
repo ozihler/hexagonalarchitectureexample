14990f8a08448a3e20ba5ae503c2be8739686129
ozihler <oliver.zihler@zuehlke.com>
2019-02-19 17:13:29 +0100

    initial setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,26 +9,25 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         Map<String, List<String>> toDoLists = new HashMap<>();
-        toDoLists.put("Daily Routine", List.of("Grocery Shopping", "Fitness"));
+        toDoLists.put("Daily Routine", new ArrayList<>(List.of("Grocery Shopping", "Fitness")));
 
         List<String> toDoList = toDoLists.get("Daily Routine");
         show(toDoList);
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         String nextLine;
         do {
             System.out.println("Enter next item to todo list:");
             nextLine = reader.readLine();
 
-            if (nextLine != null && !nextLine.isBlank()) {
-                toDoList.add(nextLine);
+            if (nextLine != null && !nextLine. isBlank()) {
+                toDoLists.get("Daily Routine").add(nextLine);
             }
 
-            toDoLists.put("Daily Routine", toDoList);
             show(toDoList);
-        } while (nextLine != null);
+        } while (nextLine != null && !nextLine. isBlank());
 
         System.out.println("Finished adding items to todo list");
     }
