05fa5f1e3c08f7ae6017041f75068cd96073d1aa
ozihler <oliver.zihler@zuehlke.com>
2019-02-19 17:07:11 +0100

    initial setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,33 +9,30 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Map<String, List> toDoLists = new HashMap<>();
-        toDoLists.put("Daily Routine", new ArrayList<>(List.of(Item.of("Grocery Shopping"), Item.of("Fitness"))));
+        Map<String, List<String>> toDoLists = new HashMap<>();
+        toDoLists.put("Daily Routine", List.of("Grocery Shopping", "Fitness"));
 
-        List<Item> toDoList = toDoLists.get("Daily Routine");
+        List<String> toDoList = toDoLists.get("Daily Routine");
         show(toDoList);
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Item nextItem;
+        String nextLine;
         do {
             System.out.println("Enter next item to todo list:");
-            String nextLine = reader.readLine();
+            nextLine = reader.readLine();
 
-            if (nextLine == null || nextLine.isBlank()) {
-                nextItem = null;
-            } else {
-                nextItem = new Item(nextLine);
+            if (nextLine != null && !nextLine.isBlank()) {
+                toDoList.add(nextLine);
             }
 
-            toDoList.add(nextItem);
             toDoLists.put("Daily Routine", toDoList);
             show(toDoList);
-        } while (nextItem != null);
+        } while (nextLine != null);
 
         System.out.println("Finished adding items to todo list");
     }
 
-    private static void show(List<Item> toDoList) {
+    private static void show(List<String> toDoList) {
         System.out.println("All items in todo list:");
         System.out.println("=======================");
         toDoList.forEach(System.out::println);
